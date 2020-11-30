fa49ca3786b82ceefdf4775ff02dead7c58ea1d4
PandaZGV2 <aaronmonis22@gmail.com>
2020-11-30 15:36:35 +0400

    updated files

--- parserdna.py
+++ parserdna.py
@@ -3,8 +3,9 @@ from array import *
 
 numseq = 0
 countdna = {'A': array('b', []), 'C': array('b', []), 'G': array('b', []), 'T': array('b', [])}
-for seqrecord in SeqIO.parse("motif_b.fasta", "fasta"):
+for seqrecord in SeqIO.parse("motif_c.fasta", "fasta"):
     seq = seqrecord.seq
+    # print(len(seq))
     count = 0
     if numseq != 0:
         for i in seq:
@@ -26,6 +27,7 @@ for seqrecord in SeqIO.parse("motif_b.fasta", "fasta"):
             #     countdna['C'][count] = 0
             #     countdna['A'][count] = 0
             count = count + 1
+        numseq=numseq+1
     else:
         for i in seq:
             countdna[i].append(1)
@@ -45,8 +47,8 @@ for seqrecord in SeqIO.parse("motif_b.fasta", "fasta"):
                 countdna['G'].append(0)
                 countdna['C'].append(0)
                 countdna['A'].append(0)
-            count = count + 1
-            numseq=1
+            # count = count + 1
+        numseq=1
 
 string = ""
 for j in range(len(countdna['A'])):
@@ -60,18 +62,21 @@ for j in range(len(countdna['A'])):
 
 print("A ", end='')
 for i in range(len(countdna['A'])):
-    print(countdna['A'][i], end='')
+    print(countdna['A'][i], end=" ")
 print("")
 print("C ", end='')
 for i in range(len(countdna['C'])):
-    print(countdna['C'][i], end='')
+    print(countdna['C'][i], end=" ")
 print("")
 print("G ", end='')
 for i in range(len(countdna['G'])):
-    print(countdna['G'][i], end='')
+    print(countdna['G'][i], end=" ")
 print("")
 print("T ", end='')
 for i in range(len(countdna['T'])):
-    print(countdna['T'][i], end='')
+    print(countdna['T'][i], end=" ")
 
 print("\n\n" + string)
+print(len(string))
+print(len(countdna['A']),len(countdna['C']),len(countdna['G']),len(countdna['T']))
+print(numseq)
